e7ef23bf7e937e5c0ba2da32100af0ee5b10d1f8
Woosang <yws1502@naver.com>
2021-10-08 23:52:52 +0900

    Added serializer(movie, rating) and viewSet(movie, raing)

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from .views import *
+
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
   path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+  queryset = Movie.objects.all()
+  serializer_class = MovieSerializer
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+  queryset = Rating.objects.all()
+  serializer_class = RatingSerializer
