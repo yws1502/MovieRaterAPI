578623b787f690201af5ce05d65a05ff4059bb50
Woosang <yws1502@naver.com>
2021-10-08 23:26:28 +0900

    Added movie, Rating model and register

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Movie(models.Model):
+  title = models.CharField(max_length=32)
+  description = models.TextField(max_length=360)
+
+
+class Rating(models.Model):
+  movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+  class Meta:
+    # 이 친구들은 한 영화에 대해 리뷰를 남기면 같은 user는 다시 리뷰를 하지 못하게 하는 것!
+    unique_together = (('user', 'movie'),)
+    index_together = (('user', 'movie'),)
